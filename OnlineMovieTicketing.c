79ef0bdfe0dd483affc96e10b3438f4f6ea89334
lyjelin <47136734+lyjelin@users.noreply.github.com>
2019-04-27 21:37:57 +0900

    Create OnlineMovieTicketing.c

---
+++ OnlineMovieTicketing.c
@@ -0,0 +1,15 @@
+#include <studio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <windows.h>
+
+struct record{
+  char bookNum[80];
+  char name[80];
+  char movie_name[80];
+  char time[80];
+  char numofGuests[80];
+  char houseNum[80];
+  char ticketType[80];
+  char totalFee[80];
+ };
